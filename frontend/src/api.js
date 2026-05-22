a44be3c232b90e14dc780aa892703407b1383069
Pankaj <kr.pankaj.098419@gmail.com>
2026-05-22 10:16:23 +0530

    fixed frontend api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "/api" });
+const api = axios.create({ baseURL: "https://gym-tracker-y7no.onrender.com/api" });
 
 // Attach token to every request
 api.interceptors.request.use((config) => {
